c3a47b7c8a3040913ac891d08727035fe3a2b282
Gajendra Pal <gajendrahitz@gmail.com>
2021-12-06 19:16:39 +0530

    Time: 24 ms (31.47%), Space: 16.8 MB (66.27%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -1,44 +1,23 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
 class Solution {
 public:
-    
-    void t(Node* root,map<int,vector<Node*>> &m,int depth)
+    void link(Node* left,Node*right)
     {
-        if(root==NULL)
+        if(left==NULL&&right==NULL)
             return;
-        m[depth].push_back(root);
-        t(root->left,m,depth+1);
-        t(root->right,m,depth+1);
+        
+        left->next=right;
+        
+        link(left->left,left->right);
+        link(left->right,right->left);
+        link(right->left,right->right);
+
     }
     Node* connect(Node* root) {
-        map<int,vector<Node*>> m;
-        t(root,m,0);
+        if(root==NULL)
+            return NULL;
         
-        for(auto &i:m)
-        {
-           for(int j=1;j<i.second.size();j++)
-           {
-               i.second[j-1]->next=i.second[j];
-           }
-        }
-        
+        link(root->left,root->right);
         return root;
+        
     }
 };
