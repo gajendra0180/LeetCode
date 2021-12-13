dc367047a358c17a96a7a6dac7e2b2e4a83e53ce
Gajendra Pal <gajendrahitz@gmail.com>
2021-12-13 09:44:33 +0530

    Time: 116 ms (67.31%), Space: 11.2 MB (73.17%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
-        struct HASH{
-	size_t operator()(const pair<int,int> &x)const{
-		return hash<int>()(x.first^x.second);
-	}
-};
-    unordered_map<pair<int,int>,int,HASH> m;
-    bool can(int sum,vector<int>&v,int i,int req)
-    {
-        if(i>=v.size())
-            return false;
-        if(sum==req)
-            return true;
-        if(sum>req)
-            return false;
-        
-        if(m.find({i,sum})!=m.end())
-            return m[{i,sum}];
-        
-        return m[{i,sum}]=can(sum+v[i],v,i+1,req)||can(sum,v,i+1,req);
-    }
-    
     bool canPartition(vector<int>& nums) {
         int sum=0;
-        for(auto i:nums)sum+=i;
-        if(sum%2==0)
+        for(auto i:nums)
+            sum+=i;
+        if(sum%2!=0)
+            return false;
+        sum/=2;
+        int n=nums.size();
+        
+        bool t[nums.size()+1][sum+1];
+        
+        for(int i=0;i<n+1;i++)
         {
-            sum/=2;
-            if(can(0,nums   ,0,sum))
-                return true;
-        }            
-        return false;
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0&&j==0)
+                    t[i][j]=true;
+                else if(i==0)
+                    t[i][j]=false;
+                else if(j==0)
+                    t[i][j]=true;   
+            }
+        }
+       for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+
+              if(nums[i-1]<=j)
+                {
+                    t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
+                }
+                else
+                {
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
+
     }
 };
