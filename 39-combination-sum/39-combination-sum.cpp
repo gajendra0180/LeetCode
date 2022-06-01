69b8ed1ff043f9feec23bb17fc9c6c5160f0d61d
Gajendra Pal <gajendrahitz@gmail.com>
2022-06-01 15:54:04 +0530

    Time: 64 ms (28.62%), Space: 18.3 MB (33.00%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>>res;
+    set<vector<int>>res;
     void combinations(int index,vector<int>&candidates,int target,vector<int> &temp){
         
         if(index>=candidates.size() || target<0)
             return;
-        
         if(target==0){
-            res.push_back(temp);
+            res.insert(temp);
             return;
         }
-        
         combinations(index+1,candidates,target,temp);        
         temp.push_back(candidates[index]);
-        combinations(index,candidates,target-candidates[index],temp);   
+        combinations(index+1,candidates,target-candidates[index],temp);      
+        combinations(index,candidates,target-candidates[index],temp);    
         temp.pop_back();
-        
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> a;
         
+        vector<int>a;
         combinations(0,candidates,target,a);
-        
-        return res;
+        vector<vector<int>>ress;
+        for(auto i:res){
+            ress.push_back(i);
+        }
+        return ress;
         
     }
 };
