906fdfa692ae26fcc28d66952d2ab90e16f2ce0e
Gajendra Pal <gajendrahitz@gmail.com>
2022-07-13 17:46:00 +0530

    Time: 1378 ms (7.50%), Space: 10.1 MB (48.02%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,24 +1,43 @@
 class Solution {
 public:
-    bool sum(int s1,int s2,int s3,int s4,vector<int>&m,int index,int sum1){
+    
+    bool can(vector<int>&m,vector<int>&s,int index,int sum){
+        
         if(index==m.size())
-            return (s1==s2&&s2==s3&&s3==s4);
-        
-        if(s1>(sum1/4) || s2>(sum1/4) || s3>(sum1/4) ||s4>(sum1/4))
+        {
+            if(s[0]==s[1]&&s[1]==s[2]&&s[2]==s[3])
+                return true;
             return false;
+        }
         
-        return sum(s1+m[index],s2,s3,s4,m,index+1,sum1)||sum(s1,s2+m[index],s3,s4,m,index+1,sum1)||sum(s1,s2,s3+m[index],s4,m,index+1,sum1)||sum(s1,s2,s3,s4+m[index],m,index+1,sum1);
+        for(int i=0;i<4;i++){
+            s[i]+=m[index];
+            
+            
+            if(s[i]>sum/4.0){
+                s[i]-=m[index];
+                continue;
+            }
+            
+            if(can(m,s,index+1,sum))
+                return true;
+            s[i]-=m[index];
+        }    
+        return false;
     }
     
-    
-    bool makesquare(vector<int>& matchsticks) {
-        int sum1=0;
-        for(auto i:matchsticks)
-            sum1+=i;
-        if(sum1%4!=0)
+    bool makesquare(vector<int>& m) {
+        int s=0;
+        sort(m.begin(),m.end(),greater<int>());
+        for(auto i:m)
+            s+=i;
+        
+        if(s%4!=0 || m.size()<4)
             return false;
-        sort(matchsticks.begin(),matchsticks.end());
-        reverse(matchsticks.begin(),matchsticks.end());
-        return sum(0,0,0,0,matchsticks,0,sum1);
+        
+        vector<int>ss{0,0,0,0};
+        
+        return can(m,ss,0,s);
+            
     }
 };
