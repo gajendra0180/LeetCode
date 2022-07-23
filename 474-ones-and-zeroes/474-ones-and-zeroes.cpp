7085e7205470ca465b5d919bb0347f8c7caf25bf
Gajendra Pal <gajendrahitz@gmail.com>
2022-07-23 09:26:30 +0530

    Time: 995 ms (7.37%), Space: 36.1 MB (31.60%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     vector<int> getCount(string s){
         int one=0,zero=0;
         for(auto i:s){
@@ -11,6 +12,7 @@ public:
         return {zero,one};
     }
     int arr[601][101][101];
+       vector<pair<int,int>> count;
     int f(vector<string>&strs,int m,int n,int index){
        
         if(m==0&&n==0 || index<0)
@@ -19,14 +21,13 @@ public:
         if(arr[index][m][n]!=-1)
             return arr[index][m][n];
         
-        vector<int>count=getCount(strs[index]);
         
-        if(count[0]>m || count[1]>n){
+        if(count[index].first>m || count[index].second>n){
             return arr[index][m][n] = f(strs,m,n,index-1);
         }
         else
         {
-            return  arr[index][m][n] = max(1+f(strs,m-count[0],n-count[1],index-1),f(strs,m,n,index-1));
+            return  arr[index][m][n] = max(1+f(strs,m-count[index].first,n-count[index].second,index-1),f(strs,m,n,index-1));
         }
     }
     
@@ -37,6 +38,11 @@ public:
                     arr[i][j][k]=-1;
                 }
             }
+        }
+     
+        for(auto i:strs){
+            vector<int>temp=getCount(i);
+            count.push_back({temp[0],temp[1]});
         }
         return f(strs,m,n,strs.size()-1);
     }
