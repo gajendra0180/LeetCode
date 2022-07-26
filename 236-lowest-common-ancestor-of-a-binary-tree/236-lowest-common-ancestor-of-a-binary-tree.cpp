cc937201fbbe60dc324c0ac50d9b204f6ca1fcf6
Gajendra Pal <gajendrahitz@gmail.com>
2022-07-26 18:25:26 +0530

    Time: 876 ms (5.09%), Space: 17.2 MB (12.08%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,28 +1,27 @@
+
 class Solution {
 public:
-    void get(TreeNode* r,TreeNode* f,vector<TreeNode*>&v,vector<TreeNode*>&ans){
-        if(!r)return;
-        if(r==f){
-            v.push_back(r);
-            ans=v;
+    void hasBoth(TreeNode* r,TreeNode* p,TreeNode *q,int &c,int &d){
+        if(r==NULL)
             return;
-        }
-        v.push_back(r);
-        get(r->left,f,v,ans);
-        get(r->right,f,v,ans);
-        v.pop_back();
+        if(r==p)
+            c=1;
+        if(r==q)
+            d=1;
+        hasBoth(r->left,p,q,c,d);
+        hasBoth(r->right,p,q,c,d);
     }
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>v1,v2,ans1,ans2;
-         get(root,p,v1,ans1);
-         get(root,q,v2,ans2);
-        int i=0;
-        for(i=0;i<min(ans1.size(),ans2.size());i++)
-            if(ans1[i]->val!=ans2[i]->val)
-                return ans1[i-1];
-        if(i==ans1.size())return ans1.back();
-        if(i==ans2.size())return ans2.back();
-        return NULL;
+        int c1=0,c2=0,c3=0,c4=0;
+        hasBoth(root->left,p,q,c1,c2);
+         hasBoth(root->right,p,q,c3,c4);
+        if(c1&&c2){
+            return lowestCommonAncestor(root->left,p,q);
+        }else if(c3&&c4){
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        else
+            return root;
     }
 };
