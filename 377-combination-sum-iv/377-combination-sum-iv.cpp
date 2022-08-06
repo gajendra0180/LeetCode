4a02ffd2b5d2ce1e8d89ae1b6927201f277dd633
Gajendra Pal <gajendrahitz@gmail.com>
2022-08-06 11:57:56 +0530

    Time: 15 ms (5.61%), Space: 7.5 MB (7.58%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int arr[201][1001];
-    int combination(vector<int>&candidates,int index,int target,vector<int>&temp){
+    int dp[201][1001];
+    int get(vector<int>&v,int index,int sum,int target){
         
-        if(target==0)
+        if(index>=v.size())
+            return 0;
+        if(sum==target)
             return 1;
         
-        if(index>=candidates.size() || target<0)
-            return 0;
+        if(dp[index][sum]!=-1)
+            return dp[index][sum];
         
-        if(arr[index][target]!=-1)
-            return arr[index][target];
-        
-        int r=0;
-        for(int i=0;i<candidates.size();i++){
-            temp.push_back(candidates[i]);
-            r+=combination(candidates,i,target-candidates[i],temp);
-            temp.pop_back();
-        }
-        return arr[index][target] = r;
-    }
-    
-    int combinationSum4(vector<int>& candidates, int target) {
-        vector<int>temp;
-        for(int i=0;i<201;i++){
-            for(int j=0;j<1001;j++){
-                arr[i][j]=-1;
+        int ans=0;
+        for(int i=0;i<v.size();i++){
+            if(v[i]+sum<=target){
+                ans+=get(v,i,sum+v[i],target);
             }
         }
-        return combination(candidates,0,target,temp);
+        return dp[index][sum] = ans;
+    }
+    
+    int combinationSum4(vector<int>& v, int target) {
+        sort(v.begin(),v.end());
+        memset(dp,-1,sizeof dp);
+        return get(v,0,0,target);
     }
 };
