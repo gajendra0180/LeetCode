8010202b03819eb314f26f6225d18b135f04908a
Gajendra Pal <gajendrahitz@gmail.com>
2022-08-11 08:08:49 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (70.19%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
+    bool get(int i,int j,vector<int>&v,int s1,int s2){
+        if(i>j)
+            return s1>s2;
+        return get(i+1,j,v,s1+v[i],s2)||get(i,j-1,v,s1,s2+v[j]);
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        return true;
+       return get(0,piles.size()-1,piles,0,0);
     }
 };
