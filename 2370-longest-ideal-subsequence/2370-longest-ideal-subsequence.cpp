25746b87e88dfe9f825c30cd26d9837724b80587
Gajendra Pal <gajendrahitz@gmail.com>
2022-08-13 22:31:21 +0530

    Time: 2451 ms (14.29%), Space: 73.6 MB (85.71%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dp[100001][123];
+int get(string &s, int &k, int index, char lastWord)
+{
+	if (index == s.size())
+		return 0;
+		
+    if(dp[index][lastWord]!=-1){
+        return dp[index][lastWord];
+    }
+    
+	char ch;
+	if (lastWord == ch)
+	{
+		return dp[index][lastWord] = max(1 + get(s, k, index + 1, s[index]), get(s, k, index + 1, ch));
+	}
+	else
+	{
+		// le skte h?
+		if (abs(s[index] - lastWord) <= k)
+		{
+			return dp[index][lastWord] = max(1 + get(s, k, index + 1, s[index]), get(s, k, index + 1, lastWord));
+		}
+		else
+			return dp[index][lastWord] =  get(s, k, index + 1, lastWord);
+	}
+}
+    
+    int longestIdealString(string s, int k) {
+       char ch;
+        for(int i=0;i<s.size();i++){
+            for(int j=0;j<=122;j++){
+                dp[i][j]=-1;
+            }
+        }
+        memset(dp,-1,sizeof dp);
+	return get(s, k, 0, ch);
+    }
+};
